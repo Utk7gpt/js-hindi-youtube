64efbac04e6042bdf7cbf9694ce15dfd2f8c683f
Utk7gpt <98682354+Utk7gpt@users.noreply.github.com>
2026-01-04 13:44:56 +0000

    datatypes

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,17 @@
+/* const accountId = 554856
+let accountEmail ="ygedeuvd@googlr.com"
+var accountPasswor = "74596"
+accountCity = "Jaipur" */
+
+ accountId = 2
+
+accountEmail ="ghihhi@googlr.com"
+accountPassword = "8542"
+accountCity = "hjkjj"
+let accounststate;
+
+
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword,accountCity,accounststate])
+
+ 
---
+++ 01_basis/02_datatypes.js
@@ -0,0 +1,21 @@
+//alert(3 + 3)// will throw error as we are using node js not document
+console.log(3+3); 
+console.log("utk")
+
+let name = 'utk'
+let age = 18
+let isloggedIn = false
+
+// Primitive Datatypes -
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standAlone value
+// undefined => whwn no value being assigned
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined)// undefined
+console.log(typeof null) // it will return object and for undefined it is undefined
