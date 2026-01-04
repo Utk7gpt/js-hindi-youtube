a68e5445ae03df1f125048108dac9ed08282addd
Utk7gpt <98682354+Utk7gpt@users.noreply.github.com>
2026-01-04 14:36:39 +0000

    conversion of datatypes

---
+++ 01_basis/03_converoper.js
@@ -0,0 +1,22 @@
+let score = true
+console.log(typeof score)
+let valNumber = Number(score)
+console.log(typeof valNumber)
+console.log(valNumber)
+
+// "33" => 33
+// "33asd" => NAN
+// true =>1; false =>0
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console. log (booleanIsLoggedIn) ;
+
+// 1 =>True; 0=>False
+// "" =>false
+// "rtyh" =>True
+
+let somenumber = 44
+let strnumber = String(somenumber)
+console.log(typeof(strnumber))
