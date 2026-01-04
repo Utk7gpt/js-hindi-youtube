d92d1843d72d93027da292ab8c46b20985c91fb8
Utk7gpt <98682354+Utk7gpt@users.noreply.github.com>
2026-01-04 15:06:43 +0000

    conversionsooperations

--- 01_basis/03_converoper.js
+++ 01_basis/03_converoper.js
@@ -4,19 +4,54 @@ let valNumber = Number(score)
 console.log(typeof valNumber)
 console.log(valNumber)
 
-// "33" => 33
-// "33asd" => NAN
-// true =>1; false =>0
+ /* "33" => 33
+ "33asd" => NAN
+ true =>1; false =>0 */
 
 let isLoggedIn = "1"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console. log (booleanIsLoggedIn) ;
 
-// 1 =>True; 0=>False
-// "" =>false
-// "rtyh" =>True
+ /*1 =>True; 0=>False
+ "" =>false
+ "rtyh" =>True*/
 
 let somenumber = 44
 let strnumber = String(somenumber)
-console.log(typeof(strnumber))
+console.log(typeof(strnumber))//
+
+//******************** Operations **************/
+
+let value = 3
+let negValue = -value
+ console. log (negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2 ** 3);
+// console.log(2/3);
+// console.log (2%3);
+
+ let str1 = "rtyu "
+ let str2 ="fdhh"
+ let str3 = str1+str2
+ console.log(str3)
+ console.log("1" + 2 + 2) // 122
+ console.log(1 + 2 +"2") //
+
+// console.log(+true) // 1
+// console.log(+"") // 0 
+// console.log(true+) // error
+
+ let gamecounter = 100
+ ++gamecounter;
+ console.log(gamecounter)
+
+let x = 3
+const y = ++x
+console.log(x)
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.//
