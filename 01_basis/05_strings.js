dd47540ec33ecb1771e0de9fd70e06855d1589e0
Utk7gpt <98682354+Utk7gpt@users.noreply.github.com>
2026-01-05 17:30:03 +0000

    strings in js

---
+++ 01_basis/05_strings.js
@@ -0,0 +1,94 @@
+// const name = "hitesh"
+// const repoCount = 50
+
+// // console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('hitesh-hc-com')
+
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2)); // gives character at index 2
+// console.log(gameName.indexOf('t')); // gives index of specefic letter
+
+// const newString = gameName.substring(0, 4) // return letters in desired range
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4) // can take negative value in slicing
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+// text = "hello "
+// text2 = "world"
+
+// let text3 = text.concat(text2);
+// console.log(text3);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12);
+// console.log(part);
+
+// let text1 = "Hello World!";
+// let text2 = text1.toUpperCase();
+// console.log(text2);
+
+
+// let text1 = "      Hello World!      ";
+// let text2 = text1.trim();
+// console.log(text2);
+
+// let text = "5";
+// let padded = text.padStart(4,"0");
+// console.log(padded);  // it is a string method
+
+// let numb = 5;
+// let text = numb.toString();
+// let padded = text.padStart(4,"0");
+// console.log(padded); // can also padend to add value to end
+
+// let text = "Hello world!";
+// let result = text.repeat(2)
+// console.log(result);
+
+// let text = "Please locate where 'locate' occurs!";
+// let asd = text.lastIndexOf("locate", 15);
+// console.log(asd);
+
+
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
+// console.log(index);
+
+// let text = "Please locate where 'locate' occurs!";
+// let asd = text.search("locate");
+// console.log(asd);
+
+let text = "The rain in SPAIN stays mainly in the plain";
+let asd = text.match("ain");
+console.log(asd); //The match() method returns an array containing the results of matching a string against a string (or a regular expression).
+
+// let text = "Hello world, welcome to the universe.";
+// let asd= text.includes("world");
+// console.log(asd);
+
+
+
+
+
+
